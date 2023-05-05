b08e41546d70ca0c26276d9f1af8aba080f2e45f
murashko12 <45760964+murashko12@users.noreply.github.com>
2023-05-05 23:34:34 +0300

    sth needs fix

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 // Import stylesheets
 import './style.css';
 
+const nameInput = document.getElementById('#name');
+const phoneInput = document.getElementById('#phone');
+const addBtn = document.getElementById('#addBtn');
+
+function addData() {
+  // event.preventDefault();
+  const newPerson = {
+    name: nameInput.value,
+    phone: phoneInput.value,
+  };
+  const newBlock = document.createElement('div');
+  const cardContainer = document.getElementsByClassName('.container');
+  newBlock.classList.add('card');
+  newBlock.innerHTML = `<div class="card__field">${nameInput.value}</div><div class="card__field">${phoneInput.value}</div>`;
+  cardContainer.appendChild(newBlock);
+}
+
+addBtn.addEventListener('click', addData);
